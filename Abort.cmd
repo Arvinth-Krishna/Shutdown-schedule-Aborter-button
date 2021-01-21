1487df11331719340045ee97380e50bf9ee1205f
Arvinth Krishna <49812701+Arvinth-Krishna@users.noreply.github.com>
2021-01-21 11:47:52 +0530

    Add files via upload

---
+++ Abort.cmd
@@ -0,0 +1,10 @@
+echo off
+echo.
+echo.
+echo *******************************************************
+echo              Aborting the shutdown schedule.
+echo *******************************************************
+echo.
+
+shutdown /a
+timeout 3
